06346e489caa32db399eb62e6377326e29cba89b
Tory <tory.bekeshko@gmail.com>
2024-03-12 23:20:31 +0200

    sticky header

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,12 @@
 $(function () {
   var mixer = mixitup('.category')
 });
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.header');
+  if (window.scrollY > 0) {
+    header.style.padding = '30px 0'; 
+  } else {
+    header.style.padding = '60px 0'; 
+  }
+});
