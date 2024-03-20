a9877adce93cf7a1ef9000d674c1f505687b1715
Tory <tory.bekeshko@gmail.com>
2024-03-20 15:35:39 +0200

    Changes made

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ function styles(){
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'app/js/mixitup.min.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
